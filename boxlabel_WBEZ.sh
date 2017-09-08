3ec081b8553f0447b6f6eba4e9c6fb47706885d5
Spencer Wright <snwright@gmail.com>
2017-09-08 10:39:31 -0500

    created wbez label printer

---
+++ boxlabel_WBEZ.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script creates a label that's used for the outside of TPR's 
+# shipping box.
+#
+# Usage: $ boxlabel.sh <frequency> <serial number>
+
+# check number of arguments and give feedback if it's not 2
+#if [ "$#" -ne 2 ]; then
+#  echo "Wrong number of arguments!"
+#  echo "Usage: $ boxlabel.sh <frequency> <serial number>" >&2
+#  exit 1
+#fi
+
+
+# create text image
+convert -density 300 -pointsize 12 -font \
+/usr/share/fonts/truetype/msttcorefonts/Courier_New_Bold.ttf \
+-size 637.5x1200 -gravity North \
+label:'\n\nYour Public Radio\nis tuned to\nWBEZ 91.5 Chicago\n\n\nEnjoy :)\n\n\n--------------------' \
+/home/pi/ops_tools/temp/text.png
+
+# create QR code for serial number
+qrencode -o /home/pi/ops_tools/temp/sn.png "sample"
+
+# resize QR code
+convert -resize 300% /home/pi/ops_tools/temp/sn.png /home/pi/ops_tools/temp/sn.png
+
+# merge two images into one
+convert /home/pi/ops_tools/temp/text.png /home/pi/ops_tools/temp/sn.png -gravity center -geometry +0+300 -composite /home/pi/ops_tools/temp/label.png
+
+# print the result
+lpr -P DYMO_LabelWriter_450_Turbo /home/pi/ops_tools/temp/label.png
+
+# delete all the temp files
+rm -rf /home/pi/ops_tools/temp/*
