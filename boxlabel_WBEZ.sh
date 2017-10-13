6836e7aa4e6b5d8330251091d66a46ffea73a6ba
Spencer Wright <snwright@gmail.com>
2017-10-12 21:56:38 -0400

    wbez label script update

--- boxlabel_WBEZ.sh
+++ boxlabel_WBEZ.sh
@@ -12,14 +12,6 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-
-# create text image
-convert -density 300 -pointsize 12 -font \
-/usr/share/fonts/truetype/msttcorefonts/Courier_New_Bold.ttf \
--size 637.5x1200 -gravity North \
-label:'\n\nYour Public Radio\nis tuned to\nWBEZ 91.5 Chicago\n\n\nEnjoy :)\n\n\n--------------------' \
-/home/pi/ops_tools/temp/text.png
-
 # create QR code for serial number
 qrencode -o /home/pi/ops_tools/temp/sn.png "$1"
 
@@ -27,7 +19,14 @@ qrencode -o /home/pi/ops_tools/temp/sn.png "$1"
 convert -resize 300% /home/pi/ops_tools/temp/sn.png /home/pi/ops_tools/temp/sn.png
 
 # merge two images into one
-convert /home/pi/ops_tools/temp/text.png /home/pi/ops_tools/temp/sn.png -gravity center -geometry +0+300 -composite /home/pi/ops_tools/temp/label.png
+convert /home/pi/ops_tools/data/WBEZ_label.png /home/pi/ops_tools/temp/sn.png -gravity center -geometry +0+300 -composite /home/pi/ops_tools/temp/label.png
+
+# print the result
+lpr -P DYMO_LabelWriter_450_Turbo /home/pi/ops_tools/temp/label.png
+
+# delete all the temp files
+rm -rf /home/pi/ops_tools/temp/*
+ /home/pi/ops_tools/temp/text.png /home/pi/ops_tools/temp/sn.png -gravity center -geometry +0+300 -composite /home/pi/ops_tools/temp/label.png
 
 # print the result
 lpr -P DYMO_LabelWriter_450_Turbo /home/pi/ops_tools/temp/label.png
