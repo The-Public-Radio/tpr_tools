dd7e76e00cc71a7c802aa7dcda6fb8f1204da7d3
Spencer Wright <snwright@gmail.com>
2017-10-24 13:25:12 -0400

    created manual perso/program/test script

---
+++ program_one_radio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# usage:
+# $ program_one_radio.sh <freq>
+
+sudo /home/pi/ops_tools/perso.py foo $1 US 
+
+sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/ops_tools/data/TPR_firmware.hex -U eeprom:w:/home/pi/ops_tools/temp/eeprom
+
+sudo /home/pi/fm_transmitter/fm_transmitter -f $1 /home/pi/ops_tools/data/1KTest_1s.wav
