0e5cf889636647d47a7fef0249335b49e3e8d000
Spencer Wright <snwright@gmail.com>
2017-11-09 00:16:24 -0500

    made lookup_shipment, tested it, made packing_list.sh
    i think that packinglist.sh and lookup_shipment.sh should be purged,
    but i want to do a little testing first.

--- lookup_shipment.sh
+++ lookup_shipment.sh
@@ -61,30 +61,27 @@ order_id=$(echo -n $shipment | jq '.order_id');
 
 order=$(curl -s -H "$headers" $url/orders/$order_id)
 name=$(echo -n $order | jq '.name')
+name=$(eval echo $name)
 street_address_1=$(echo -n $order | jq '.street_address_1')
+street_address_1=$(eval echo $street_address_1)
 street_address_2=$(echo -n $order | jq '.street_address_2')
+street_address_2=$(eval echo $street_address_2)
 city=$(echo -n $order | jq '.city')
+city=$(eval echo $city)
 state=$(echo -n $order | jq '.state')
+state=$(eval echo $state)
 postal_code=$(echo -n $order | jq '.postal_code')
+postal_code=$(eval echo $postal_code)
 order_source=$(echo -n $order | jq '.order_source')
-
-#echo "order is" $order
-#echo "name is" $name
-#echo "street_address_1 is" $street_address_1
-#echo "street_address_2 is" $street_address_2
-#echo "city is" $city
-#echo "state is" $state
-#echo "postal_code is" $postal_code
-#echo "order_source is" $order_source
-
+order_source=$(eval echo $order_source)
 
 radios=($(curl -s -H "$headers" $url/shipments/$shipment_id/radios | jq -c '[.data[] | .frequency][]' | sed 's/"//g'))
 
 radio_count=${#radios[@]}
 #echo "radio_count is" $radio_count
 
-output="$order_source order_number $name $street_address_1 $street_address_2 $city $state $postal_code 'a very very very very very long message' ${radios[0]} ${radios[1]} ${radios[2]}"
-echo $output
+#output="$order_source order_number $name $street_address_1 $street_address_2 $city $state $postal_code 'a very very very very very long message' ${radios[0]} ${radios[1]} ${radios[2]}"
+#echo $output
 
 
 
