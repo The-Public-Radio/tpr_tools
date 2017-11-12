ae14a6cd596b63435322289780672b0be0866b4f
Spencer Wright <snwright@gmail.com>
2017-11-12 16:21:50 -0500

    uncommon_goods background

--- boxlabel.sh
+++ boxlabel.sh
@@ -29,6 +29,20 @@ if [ $3 != "KUER" ] && [ $3 != "WMBR" ] && [ $3 != "WBEZ" ]; then
 	# merge two images into one
 	convert /home/pi/ops_tools/temp/background.png /home/pi/ops_tools/temp/sn.png \
 	-gravity center -geometry +0+300 -composite /home/pi/ops_tools/temp/label.png
+elif [ $3 = "uncommon_goods" ]; then
+	# create text image
+	convert -density 300 -pointsize 12 -font \
+	/usr/share/fonts/truetype/msttcorefonts/Courier_New_Bold.ttf \
+	-size 637.5x1200 -gravity North \
+	label:'\n\n\nYour Public Radio\nis tuned to\n'"$1"' MHz\n\n\nEnjoy :)' \
+	/home/pi/ops_tools/temp/background.png
+	# merge with uncommon_goods logo
+	convert /home/pi/ops_tools/data/label_uncommon_goods.png /home/pi/ops_tools/temp/background.png \
+	-composite /home/pi/ops_tools/temp/background.png
+	# merge two images into one
+	convert /home/pi/ops_tools/temp/background.png /home/pi/ops_tools/temp/sn.png \
+	-gravity center -geometry +0+300 -composite /home/pi/ops_tools/temp/label.png
+
 
 # else, i.e. if order_source IS a radio station
 else
