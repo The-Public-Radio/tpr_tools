ce7f54b335009f522c1b4178b689afde74e6d436
ZachDunham <zachary.dunham@gmail.com>
2017-12-03 13:15:34 -0500

    update program one radio
    updated script to use most recent firmware. need a better way to track this moving forward

--- program_one_radio.sh
+++ program_one_radio.sh
@@ -4,6 +4,6 @@
 
 sudo /home/pi/ops_tools/perso.py foo $1 US 
 
-sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/ops_tools/data/TPR_firmware.hex -U eeprom:w:/home/pi/ops_tools/temp/eeprom
+sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/ops_tools/data/3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex -U eeprom:w:/home/pi/ops_tools/temp/eeprom
 
 sudo /home/pi/fm_transmitter/fm_transmitter -f $1 /home/pi/ops_tools/data/1KTest_1s.wav
