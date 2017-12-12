e96d9229202ea5da7a5e08fb497798eb85423583
Spencer Wright <snwright@gmail.com>
2017-12-11 22:31:06 -0500

    make it actually do some stuff

--- inventory_publish.py
+++ inventory_publish.py
@@ -14,19 +14,38 @@ spreadsheet = c.open('PR9450 Part Usage')
 
 stored_events_file = '/home/pi/ops_tools/data/stored_inventory_events.csv'
 
+def record_assemble(timestamp, user):
+	PCB = spreadsheet.worksheet("PR9027")
+	PCB.append_row(["radio_assemble", timestamp, "-1", user])
+	lid = spreadsheet.worksheet("PR1014")
+	lid.append_row(["radio_assemble", timestamp, "-1", user])
+	screw = spreadsheet.worksheet("PR2039")
+	screw.append_row(["radio_assemble", timestamp, "-4", user])	
+	knob = spreadsheet.worksheet("PR2036")
+	knob.append_row(["radio_assemble", timestamp, "-1", user])
+	mech_assy = spreadsheet.worksheet("PR2040")
+	mech_assy.append_row(["radio_assemble", timestamp, "+1", user])
+
+def record_fulfill(timestamp, user):
+	antenna = spreadsheet.worksheet("PR2034")
+	antenna.append_row(["radio_fulfill", timestamp, "-1", user])
+	box = spreadsheet.worksheet("PR2500")
+	box.append_row(["radio_fulfill", timestamp, "-1", user])
+	jar = spreadsheet.worksheet("PR1016")
+	jar.append_row(["radio_fulfill", timestamp, "-1", user])
+	mech_assy = spreadsheet.worksheet("PR2040")
+	mech_assy.append_row(["radio_assemble", timestamp, "-1", user])
+
 with open(stored_events_file, 'rwb') as f:
 	csvreader = csv.reader(f, delimiter = ',')
 	next(csvreader, None)
 	for row in csvreader:
-		print row[0]
 		if (row[0] == 'assemble'):
-			print 'We\'ve got an assemble row!'
-			print 'Looks like', row[2], ' assembled a radio.'
-			# do some stuff
+			print 'Looks like', row[2], 'assembled a radio at' row[1]
+			record_assemble(row[1], row[2])
 		elif (row[0] == 'fulfill'):
-			# do other stuff
-			print 'We\'ve got a fulfill row!'
-			print 'Looks like', row[2], ' fulfilled a radio.'
+			print 'Looks like', row[2], 'fulfilled a radio at' row[1]
+			record_fulfill(row[1], row[2])
 		else:
 			print 'ERROR - CSV file is not properly formatted.'
 			print 'Exiting.'
