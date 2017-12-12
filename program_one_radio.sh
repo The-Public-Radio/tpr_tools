06922c2b411ab459c3066e82a90cf3da9b5c550c
Spencer Wright <snwright@gmail.com>
2017-12-12 14:45:29 -0500

    not null!

--- program_one_radio.sh
+++ program_one_radio.sh
@@ -8,7 +8,7 @@ sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/o
 
 sudo /home/pi/fm_transmitter/fm_transmitter -f $1 /home/pi/ops_tools/data/1KTest_1s.wav
 
-if [[ -z "$2" ]] && [[ $2 = "production" ]]; then
+if [[ -n "$2" ]] && [[ $2 = "production" ]]; then
 	echo 'Removing parts from inventory...'
 	sudo python /home/pi/ops_tools/inventory_event.py fulfill
 else
