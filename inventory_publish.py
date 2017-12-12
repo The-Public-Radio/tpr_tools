bf8d86957dc3772fca0ab538da3fbbff5f53b379
Spencer Wright <snwright@gmail.com>
2017-12-11 23:56:44 -0500

    ready to test

--- inventory_publish.py
+++ inventory_publish.py
@@ -60,20 +60,38 @@ PR2500 = []
 PR1016 = []
 PR2040 = []
 
+#worksheets = ['PR9027', 'PR1024', 'PR2039', 'PR2036', 'PR2040', 'PR2034', 'PR2500', 'PR1016', 'PR2040']
+worksheets = ['PR9040']
+
 with open(stored_events_file, 'rwb') as f:
 	csvreader = csv.reader(f, delimiter = ',')
 	next(csvreader, None)
 	for row in csvreader:
 		if (row[0] == 'assemble'):
 			print 'Looks like', row[2], 'assembled a radio at', row[1]
-			#record_assemble(row[1], row[2])
+			PR9027.append([row[0], row[1], '-1', row[2]])
+			PR1024.append([row[0], row[1], '-1', row[2]])
+			PR2039.append([row[0], row[1], '-4', row[2]])
+			PR2036.append([row[0], row[1], '-1', row[2]])
+			PR2040.append([row[0], row[1], '1', row[2]])
 		elif (row[0] == 'fulfill'):
 			print 'Looks like', row[2], 'fulfilled a radio at', row[1]
-			#record_fulfill(row[1], row[2])
+			PR2034.append([row[0], row[1], '-1', row[2]])
+			PR2500.append([row[0], row[1], '-1', row[2]])
+			PR1016.append([row[0], row[1], '-1', row[2]])
+			PR2040.append([row[0], row[1], '-1', row[2]])
 		else:
 			print 'ERROR - CSV file is not properly formatted.'
 			print 'Exiting.'
 			sys.exit(1)
+
+print PR9040
+
+#for w in worksheets:
+#	sheet = spreadsheet.worksheet(w)
+#	append_rows('%s', w[] % w)
+
+
 
 print 'Done!'
 sys.exit(0)
