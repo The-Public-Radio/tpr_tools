7a523dfc5c2352a5d8a5c539590ee43f06dac218
ZachDunham <zachary.dunham@gmail.com>
2018-02-21 22:57:35 -0500

    Test board programming script
    This is a script to simply programming a pcb using a button on the jig to trigger avrdude. It takes two arguments: firmware and eeprom, which are defaulting to our current firmware image, and temp directory for eeprom.
    
    Couple notes.
    
    * I tested this with an AVRISPMK II. My amazon $15 avrisp actually provided 5V without me knowing. The purpose of this board is that 5V is provided externally through a supply and passed to the DUT via test point T2 and ground. Powering the board through the ISP header defeats the purpose of the board.
    
    * This script is using GPIO 2 (pin 3) on the Pi. I think that this is open on the Pi in our jig. If it's not then lines 39 and 42 need to be updated accordingly.

--- program_radio.py
+++ program_radio.py
@@ -1,16 +1,47 @@
+import sys, os
 import RPi.GPIO as GPIO
 import time
+from getopt import getopt, GetoptError
+
+
+firmware = '3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex'
+eeprom = 'eeprom'
+
+option_list='f:e:'
+
+
+def usage():
+	print r'''Usage: program_radioTEST [-f <firmware>] [-e <n>] [-d <n>] [-s <n>] [-v <n>]
+		[-m [-S <sn>] [-T <ts>] [-C <campaign>]]
+		-f <s>	Specify the string in MHz (default = 3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex)
+		-e <s>	Specify the eeprom (default = eeprom)'''
+	sys.exit(1)
+
+try:
+	opts, args = getopt(sys.argv[1:], option_list)
+except GetoptError as err:
+	print str(err)
+	usage()
+
+for o, a in opts:
+	if o == '-f':
+		firmware = str(a)
+	elif o == '-e':
+		eeprom = str(a)
+	else:
+		usage()
 
 GPIO.setmode(GPIO.BCM)
 
-# Pin that our button is connected to. I think that GPIO 2 is open, currently
+# Pin that our button is connected to. I think that GPIO 2 is open currently. Note, this is broadcom numbering.
+# See https://pinout.xyz/ for pinout 
 # Set this pin as input and enable internal pull up resistor. Our button is active low.  
 GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(2)
     if input_state == False:
-        sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/ops_tools/data/3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex -U eeprom:w:/home/pi/ops_tools/temp/eeprom
-        # debounce button 
+    	#print "programming"
+    	exit = os.system('sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/ops_tools/data/'+firmware+ ' -U eeprom:w:/home/pi/ops_tools/temp/'+eeprom)
         time.sleep(0.5) 
         exit()
