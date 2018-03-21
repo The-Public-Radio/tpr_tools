beb724a0207e9775e20131fd2f0ff17385641cc2
Spencer Wright <snwright@gmail.com>
2018-03-21 13:12:54 -0400

    started sketching out the path forward
    getopt does not seem to do what we need, and argparse looks more
    comprehensive. switching to that.
    
    making default_firmware and default_eeprom a bit easier to handle. this
    will allow the user to specify a full path to firmware and/or eeprom
    files if they want to override.
    
    sketched out how our conditionals should work to give the exit codes we
    need.

--- program_radio.py
+++ program_radio.py
@@ -1,35 +1,23 @@
 import sys, os
 import RPi.GPIO as GPIO
 import time
-from getopt import getopt, GetoptError
+import argparse
+
+default_firmware = '/home/pi/ops_tools/data/3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex'
+default_eeprom = 'eeprom'
 
 
-firmware = '3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex'
-eeprom = 'eeprom'
+#get arguments
+# if --f is specified, set firmware to be the firmware argument
+# 	if the firmware that was specified doesn't exist, exit 1
+# else set firmware to default_firmware
+# if --e is specified, set eeprom to be the eeprom argument
+# 	if the eeprom that was specified doesn't exist, exit 1
+# else set eeprom to default_eeprom
 
-option_list='f:e:'
 
 
-def usage():
-	print r'''Usage: program_radioTEST [-f <firmware>] [-e <n>] [-d <n>] [-s <n>] [-v <n>]
-		[-m [-S <sn>] [-T <ts>] [-C <campaign>]]
-		-f <s>	Specify the string in MHz (default = 3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex)
-		-e <s>	Specify the eeprom (default = eeprom)'''
-	sys.exit(1)
 
-try:
-	opts, args = getopt(sys.argv[1:], option_list)
-except GetoptError as err:
-	print str(err)
-	usage()
-
-for o, a in opts:
-	if o == '-f':
-		firmware = str(a)
-	elif o == '-e':
-		eeprom = str(a)
-	else:
-		usage()
 
 GPIO.setmode(GPIO.BCM)
 
@@ -42,6 +30,8 @@ while True:
     input_state = GPIO.input(2)
     if input_state == False:
     	#print "programming"
-    	exit = os.system('sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/ops_tools/data/'+firmware+ ' -U eeprom:w:/home/pi/ops_tools/temp/'+eeprom)
-        time.sleep(0.5) 
-        exit()
+    	avrdude_exit = os.system('sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:'+firmware+ ' -U eeprom:w:'+eeprom)
+        #if avrdude_exit = 0:
+        	# notify the user that the programming succeeded and exit 0
+        #else
+        	# notify the user that the programming failed and give a semantic exit code
