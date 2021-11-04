a9de920ba5e96b2936e9681450dad70c178d49b8
Spencer Wright <snwright@gmail.com>
2021-11-03 22:47:44 -0400

    adding timeout

--- program_one_radio.sh
+++ program_one_radio.sh
@@ -7,4 +7,4 @@ sudo /home/pi/ops_tools/perso.py foo $1 US
 sudo avrdude -P usb -c avrispmkii -p attiny25 -B 5 -b 9600 -U flash:w:/home/pi/ops_tools/data/3044a9415d9b2becb2ee44bd759a0c9f6be66109.hex -U eeprom:w:/home/pi/ops_tools/temp/eeprom
 
 #sudo /home/pi/fm_transmitter/fm_transmitter -f $1 /home/pi/ops_tools/data/1KTest_5s.wav
-sudo /home/pi/PiFmRds/src/pi_fm_rds -freq $1 -audio /home/pi/ops_tools/data/Test_Tone2.wav
+timeout 5s sudo /home/pi/PiFmRds/src/pi_fm_rds -freq $1 -audio /home/pi/ops_tools/data/Test_Tone2.wav
